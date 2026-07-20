ba135289e7c41b55656b95fa759b42448a08626d
Steimntz M. de Figueiredo <steimntz@gmail.com>
2026-07-20 18:05:36 -0400

    adds day caculation and telegram message

--- reminder.py
+++ reminder.py
@@ -1,15 +1,24 @@
 import os
 import requests
+from datetime import date, timedelta
 
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
-message = f"👶 Today begins week {weeks}!"
+DUE_DATE = date(2027, 3, 8)
+START_DATE = DUE_DATE - timedelta(weeks=40)
 
-requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    json={
-        "chat_id": CHAT_ID,
-        "text": message,
-    },
-)
+today = date.today()
+
+weeks = (today - START_DATE).days // 7
+days = (today - START_DATE).days
+
+if days % 7 == 0:
+    message = f"👶 Today begins week {weeks}!"
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        json={
+            "chat_id": CHAT_ID,
+            "text": message,
+        },
+    )
